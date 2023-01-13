76fadbb87b0aad51a2f42c7c9f5ed927e8b6f96d
Adil Taichinov <adil87t@gmail.com>
2023-01-14 02:37:00 +0600

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+import hexlet.code.games.Greet;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.main();
+        menu();
+    }
+
+    public static void menu() {
+        Scanner scanUserChoice = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int userChoice = scanUserChoice.nextInt();
+
+        if (userChoice == 1) {
+            Greet.main();
+        }
+        if (userChoice == 2) {
+            Even.main();
+        }
+        if (userChoice == 3) {
+            Calc.main();
+        }
+        if (userChoice == 0) {
+            System.exit(0);
+        }
     }
 }
